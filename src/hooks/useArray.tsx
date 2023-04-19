23ad1377d8d2812de6ec72498350a82e3d2ba246
truekendor <sadqwerty000@gmail.com>
2023-04-20 02:15:56 +0300

    Initialize project using Create React App

---
+++ src/hooks/useArray.tsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+
+export default function useArray<T>(defaultValue: T) {
+    const [array, setArray] = useState(defaultValue);
+
+    function update(index: number, newElement: T) {
+        setArray((a) => [
+            ...a.slice(0, index),
+            newElement,
+            ...a.slice(index + 1, a.length - 1),
+        ]);
+    }
+
+    function remove(index: number) {
+        setArray((a) => [
+            ...a.slice(0, index),
+            ...a.slice(index + 1, a.length - 1),
+        ]);
+    }
+
+    function clear() {
+        setArray([]);
+    }
+
+    function push(element: any) {
+        setArray((a) => [...a, element]);
+    }
+
+    function set(a: T[]) {
+        setArray([...a]);
+    }
+
+    return { array, remove, clear, push, update, set };
+}
